360c88ec07c80a05de067c8f0d095ee43a2e360e
matesuiso <matesuiso@hotmail.com>
2018-06-02 19:10:36 -0300

    insertar ninio

--- db/NinioDAO.java
+++ db/NinioDAO.java
@@ -2,6 +2,8 @@ package db;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -37,5 +39,44 @@ public class NinioDAO {
             return Collections.emptyList();
         }
     }
+
+    public static Boolean insertarNinio(Ninio nuevoNinio) {
+      Connection conexion = Database.getInstance().getConnection();
+      try {
+         conexion.setAutoCommit(false);
+      } catch (SQLException e) { }
+      Boolean resul = true;
+      PreparedStatement ps;
+
+      try {
+         ps = conexion.prepareStatement("INSERT INTO persona (nombre, apellido, dni) VALUES (?, ?, ?)");
+         ps.setString(1, nuevoNinio.nombre);
+         ps.setString(2, nuevoNinio.apellido);
+         ps.setString(3, nuevoNinio.dni);
+
+         ps.executeQuery();
+
+
+         ps = conexion.prepareStatement("INSERT INTO nino (dni, telefono, fecha_nacimiento) VALUES (?, ?, ?)");
+         ps.setString(1, nuevoNinio.dni);
+         ps.setString(2, nuevoNinio.telefono);
+         ps.setString(3, nuevoNinio.fechaNacimiento);
+
+         ps.executeQuery();
+
+         conexion.commit();
+      } catch (SQLException e) {
+         try {
+            conexion.rollback();
+         } catch (SQLException e1) { }
+         resul = false;
+      } finally {
+         try {
+            conexion.setAutoCommit(true);
+         } catch (SQLException e) { }
+      }
+
+      return resul;
+   }
 
 }
