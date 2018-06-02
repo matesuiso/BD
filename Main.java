b0a9070b2159b19640a3750c2e92576d1974dac7
matesuiso <matesuiso@hotmail.com>
2018-06-02 17:10:02 -0300

    asd

--- Main.java
+++ Main.java
@@ -1,15 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
    public static void main(String[] args) {
-      int i = 0;
+      int opc = 0;
+      Scanner sc = new Scanner(System.in);
 
-      while !(i == 9) {
+      while (!(opc == 9)) {
          System.out.println("Seleccione una operacion con un numero");
          System.out.println("");
          System.out.println("(1) Insertar un ninio");
          System.out.println("(2) Eliminar un ninio");
          System.out.println("(3) Listar ninios con las colonias que asisten");
-         System.out.println();
+         System.out.println("");
+         System.out.println("(9) Salir");
+         System.out.println("");
 
-      }
+         opc = sc.nextInt();
+
+         if (opc == 1) {
+            Main.insertar();
+         }
+         if (opc == 2) {
+            Main.eliminar();
+         }
+         if (opc == 3) {
+            Main.listar();
+         }
+
+      }  // end while
    }
 }
