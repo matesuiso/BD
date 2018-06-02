c0be60f9dceebe38e1a470afee06b663ad420b0a
matesuiso <matesuiso@hotmail.com>
2018-06-02 16:58:37 -0300

    esqueleto

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main {
+   public static void main(String[] args) {
+      int i = 0;
+
+      while !(i == 9) {
+         System.out.println("Seleccione una operacion con un numero");
+         System.out.println("");
+         System.out.println("(1) Insertar un ninio");
+         System.out.println("(2) Eliminar un ninio");
+         System.out.println("(3) Listar ninios con las colonias que asisten");
+         System.out.println();
+
+      }
+   }
+}
