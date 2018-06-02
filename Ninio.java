145ac52b1e2d2d8b7612a307d9c871c460d88d0f
matesuiso <matesuiso@hotmail.com>
2018-06-02 17:58:03 -0300

    asd

---
+++ Ninio.java
@@ -0,0 +1,19 @@
+public class Ninio {
+   String nombre;
+   String apellido;
+   String dni;
+   String telefono;
+   String fecha;
+
+   public Ninio(String nombre, String v2, String v3, String v4, String v5) {
+      this.nombre = nombre;
+      this.apellido = apellido;
+      this.dni = dni;
+      this.telefono = telefono;
+      this.fecha = fecha;
+   }
+
+   
+
+
+}
