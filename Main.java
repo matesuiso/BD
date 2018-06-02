40c3308b2c11279345c6446401eb7b3560c11410
Mariano Córdoba <marianocordoba039@gmail.com>
2018-06-02 17:57:17 -0300

    Archivo de configuración de la conexión a la base de datos

--- Main.java
+++ Main.java
@@ -1,32 +1,15 @@
-import java.util.Scanner;
-
 public class Main {
    public static void main(String[] args) {
-      int opc = 0;
-      Scanner sc = new Scanner(System.in);
+      int i = 0;
 
-      while (!(opc == 9)) {
+      while !(i == 9) {
          System.out.println("Seleccione una operacion con un numero");
          System.out.println("");
          System.out.println("(1) Insertar un ninio");
          System.out.println("(2) Eliminar un ninio");
          System.out.println("(3) Listar ninios con las colonias que asisten");
-         System.out.println("");
-         System.out.println("(9) Salir");
-         System.out.println("");
+         System.out.println();
 
-         opc = sc.nextInt();
-
-         if (opc == 1) {
-            Main.insertar();
-         }
-         if (opc == 2) {
-            Main.eliminar();
-         }
-         if (opc == 3) {
-            Main.listar();
-         }
-
-      }  // end while
+      }
    }
 }
--- db/Database.java
+++ db/Database.java
@@ -1,6 +1,9 @@
 package db;
 
 import java.sql.*;
+import java.util.Properties;
+import java.io.FileInputStream;
+import java.io.IOException;
 
 public class Database {
 
@@ -9,15 +12,37 @@ public class Database {
     private Connection connection;
 
     private Database() {
+        Properties props = new Properties();
+        FileInputStream fis = null;
+
+        try {
+            fis = new FileInputStream("./database.properties");
+            props.load(fis);
+        } catch (IOException e) {
+            System.out.println("Error al obtener la configuración de la base de datos.");
+            System.exit(0);
+        } finally {
+            if (fis != null) {
+                try {
+                    fis.close();
+                    fis = null;
+                } catch (IOException e) {}
+            }
+        }
+
         try {
             Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
             connection = DriverManager.getConnection(
-                "jdbc:mysql://localhost:3306/colonia_verano",
-                "root",
-                "root"
+                "jdbc:mysql://" +
+                props.getProperty("host") + ":" +
+                props.getProperty("port") + "/" +
+                props.getProperty("name"),
+                props.getProperty("user"),
+                props.getProperty("password")
             );
         } catch (Exception e) {
-          e.printStackTrace();
+            System.out.println("Error al conectar con la base de datos.");
+            System.exit(0);
         }
     }
 
